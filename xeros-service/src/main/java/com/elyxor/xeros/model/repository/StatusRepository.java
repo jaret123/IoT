d6fac03f8a581f6c65e64153cf50d792fb82ad9c
will <will.hansen@elyxor.com>
2014-09-07 10:49:44 -0400

    Bug Fix for Status Service

--- xeros-service/src/main/java/com/elyxor/xeros/model/repository/StatusRepository.java
+++ xeros-service/src/main/java/com/elyxor/xeros/model/repository/StatusRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface StatusRepository extends CrudRepository<Status, Integer> {
-    @Query(value = "SELECT * FROM xeros_status WHERE machine_id = :machineId ORDER BY timestamp desc LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM xeros_status WHERE machine_id = :machineId ORDER BY time_stamp desc LIMIT 1", nativeQuery = true)
     public Status findByMachineId(@Param("machineId") int machineId);
 
     @Query(value = "SELECT * FROM xeros_status WHERE machine_id = :machineId ORDER BY time_stamp desc, machine_id desc LIMIT 100", nativeQuery = true)
